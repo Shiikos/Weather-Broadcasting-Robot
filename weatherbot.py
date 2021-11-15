e16150dbf386021e29bd0700c2f1d9c7e4018456
Shiikos <shiikos@outlook.com>
2021-11-15 22:40:06 +0800

    新增灾害天气报警功能

--- weatherbot.py
+++ weatherbot.py
@@ -16,6 +16,8 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 webhook = ''
 #和风天气API key
 hfapi = ''
+#灾害天气API key
+warningapi = ''
 #工作日查询接口
 workdayapi = ''
 #定义年份
@@ -96,6 +98,29 @@ def sign_gx():
         d1 = yb[datetype]['txt']
         output(d1)
 
+def warning_sh():
+    yburl = 'https://api.qweather.com/v7/warning/now'
+    value = {
+        #天气地址上海：101020100
+        'location': '101020100',
+        #和风天气API
+        'key': warningapi,
+        #语言
+        'lang': 'zh'
+    }
+    ybreq = requests.get(yburl, params=value)
+    ybjs = ybreq.json()
+    # print(ybjs)
+    for i in range(1):
+        yb = ybjs['warning']
+        if not ybjs['warning']:
+            print("无灾害预警")
+        else:
+            if '黄色' == yb[0]['level'] or '橙色' == yb[0]['level'] or '红色' == yb[0]['level']:
+                d1 = yb[0]['text']
+                output(d1)
+            else:
+                print("灾害预警等级较低")
 
 #检查是否是工作日
 def check_if_is_work_day():
@@ -113,6 +138,7 @@ def check_if_is_work_day():
         datetype = 2
         sign_sh()
         sign_gx()
+        warning_sh()
         try:
             KIM()
         except Exception:
